cb7a8b22db4d896f5197bba799283259d4a9ca56
sericaer <sericaer@outlook.com>
2022-07-28 09:58:52 +0800

    show person in chaoting

---
+++ HuangD.Relations/Person2Office.cs
@@ -0,0 +1,27 @@
+﻿using HuangD.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HuangD.Relations
+{
+    internal class Person2Office : IPerson2Office
+    {
+        public IPerson person { get; private set; }
+
+        public IOffice office { get; private set; }
+
+        public bool isCurrent => history.Last().endDate == null;
+
+        public IEnumerable<((int y, int m, int d)? startDate, (int y, int m, int d)? endDate)> history => _history;
+
+        private List<((int y, int m, int d)? startDate, (int y, int m, int d)? endDate)> _history = new List<((int y, int m, int d)? startDate, (int y, int m, int d)? endDate)>();
+
+        public Person2Office(IPerson person, IOffice office, (int y, int m, int d) date)
+        {
+            this.person = person;
+            this.office = office;
+            _history.Add((date, null));
+        }
+
+    }
+}
--- HuangD.Relations/RelationMgr.Builder.cs
+++ HuangD.Relations/RelationMgr.Builder.cs
@@ -1,6 +1,7 @@
 ﻿using HuangD.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HuangD.Relations
 {
@@ -11,8 +12,26 @@ namespace HuangD.Relations
             public static IRelationMgr Build(IList<IPerson> persons, IChaotingGroup chaoting)
             {
                 var relation = new RelationMgr();
+
+                int i = 0;
+                for(; i< chaoting.mainOffices.Count(); i++)
+                {
+                    relation.Person2OfficeStart(persons.ElementAt(i), chaoting.mainOffices.ElementAt(i), (1, 1, 1));
+                }
+
+                int j = 0;
+                for (; j < chaoting.branchOffices.Count(); j++)
+                {
+                    relation.Person2OfficeStart(persons.ElementAt(j+i), chaoting.branchOffices.ElementAt(j), (1, 1, 1));
+                }
+
                 return relation;
             }
+        }
+
+        private void Person2OfficeStart(IPerson person, IOffice office, (int year, int month, int day) date)
+        {
+            person2Offices.Add(new Person2Office(person, office, date));
         }
     }
 }
